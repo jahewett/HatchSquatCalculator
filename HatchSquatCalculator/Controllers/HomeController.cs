a11d50a3b830d4138821e1988913c4bb71bc65de
Jimi Hewett <jhewett@sonovate.com>
2017-08-23 19:55:17 +0100

    Tidied up redundant code.

--- HatchSquatCalculator/Controllers/HomeController.cs
+++ HatchSquatCalculator/Controllers/HomeController.cs
@@ -12,17 +12,8 @@ namespace HatchSquatCalculator.Controllers
             return View();
         }
 
-        public IActionResult About()
+        public IActionResult Program()
         {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
             return View();
         }
 
